c659f76bc6572b7fb12f2e80b4616e2725731906
Błażej Kotowski <kotowski.blazej@gmail.com>
2024-03-27 17:34:40 +0100

    Training parametrisation

--- train.py
+++ train.py
@@ -1,7 +1,11 @@
 import lightning as L
+from lightning.pytorch.loggers import TensorBoardLogger
+
 import torch
 torch.set_default_dtype(torch.float32)
+
 import argparse
+import os
 
 from torch.utils.data import DataLoader
 from lib.dataset_tool import AudioDataset
@@ -27,9 +31,14 @@ if __name__ == '__main__':
   parser.add_argument('--resampling_factor', type=int, default=32, help='Resampling factor for the control signal and noise bands')
   parser.add_argument('--output_path', help='Directory to save the model')
   parser.add_argument('--mixed_precision', type=bool, default=False, help='Use mixed precision')
+  parser.add_argument('--training_dir', type=str, default='training', help='Directory to save the training logs')
+  parser.add_argument('--model_name', type=str, default='noisebandnet', help='Name of the model')
+  parser.add_argument('--max_epochs', type=int, default=10000, help='Maximum number of epochs')
   config = parser.parse_args()
 
   n_signal = int(config.audio_chunk_duration * config.fs)
+
+  os.makedirs(os.path.join(config.training_dir, config.model_name), exist_ok=True)
 
   dataset = AudioDataset(
     dataset_path=config.dataset_path,
@@ -41,8 +50,10 @@ if __name__ == '__main__':
 
   train_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True)
 
-  nbn = NoiseBandNet(learning_rate=1e-3)
+  nbn = NoiseBandNet(learning_rate=config.lr)
+
+  tb_logger = TensorBoardLogger(config.training_dir, name=config.model_name)
 
   precision = 16 if config.mixed_precision else 32
-  trainer = L.Trainer(max_epochs=10000, accelerator=config.device, precision=precision, log_every_n_steps=4)
+  trainer = L.Trainer(max_epochs=config.max_epochs, accelerator=config.device, precision=precision, log_every_n_steps=4, logger=tb_logger)
   trainer.fit(model=nbn, train_dataloaders=train_loader)
