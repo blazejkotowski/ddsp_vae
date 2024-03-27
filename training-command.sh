82be3c96ab7c1716a30844054d1107c2076165dc
Błażej Kotowski <kotowski.blazej@gmail.com>
2024-03-27 14:14:21 +0100

    training command

--- training-command.sh
+++ training-command.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
-python train.sh\
+python train.py\
     --dataset_path /Users/bl/code/noisebandnet/datasets/freesound-walking/processed\
     --device cpu\
     --output_path training/
