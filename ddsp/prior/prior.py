ac1f1d5e40be0678d64af089d855915897b3ab63
Błażej Kotowski <kotowski.blazej@gmail.com>
2024-05-29 00:23:39 +0200

    The latent size doesn't have to be multiplied

--- ddsp/prior/prior.py
+++ ddsp/prior/prior.py
@@ -24,14 +24,14 @@ class Prior(L.LightningModule):
     self._lr = lr
 
     self._gru = nn.GRU(
-      input_size=latent_size*2,
+      input_size=latent_size,
       hidden_size=hidden_size,
       num_layers=num_layers,
       dropout=dropout,
       batch_first = True
     )
 
-    self._out = nn.Linear(hidden_size, latent_size*2)
+    self._out = nn.Linear(hidden_size, latent_size)
 
     self._loss = nn.MSELoss()
 
@@ -40,9 +40,9 @@ class Prior(L.LightningModule):
     Predicts the latents from the input.
 
     Args:
-      - x: torch.Tensor[batch_size, seq_len, latent_size*2], the input sequence of latents
+      - x: torch.Tensor[batch_size, seq_len, latent_size], the input sequence of latents
     Returns:
-      - out: torch.Tensor[batch_size, seq_len, latent_size*2], the predicted sequence of latents
+      - out: torch.Tensor[batch_size, seq_len, latent_size], the predicted sequence of latents
     """
     out, _ = self._gru(x)
     out = self._out(out)
