afbe7e6efc26a342a9854b80efc72546dcf6ace0
Błażej Kotowski <kotowski.blazej@gmail.com>
2024-06-21 10:34:15 +0200

    Allow the export of the last model

--- ddsp/utils.py
+++ ddsp/utils.py
@@ -1,12 +1,24 @@
 
 import os
 
-def find_checkpoint(model_directory: str, return_none: bool = False) -> str:
-  """Finds the last checkpoint recursively looking in the model directory"""
+def find_checkpoint(model_directory: str, return_none: bool = False, typ: str = 'best') -> str:
+  """
+  Finds the last checkpoint recursively looking in the model directory
+
+  Args:
+    model_directory: str, the path to the model directory
+    return_none: bool, whether to return None if no checkpoint is found
+    typ: str, the type of checkpoint to find, either 'best' or 'last'
+  Returns:
+    path: str, the path to the checkpoint
+  """
+  if typ not in ['best', 'last']:
+    raise ValueError(f"Invalid type: {typ}, supported types are: best, last")
+
   checkpoints = []
   for root, _, files in os.walk(model_directory):
     for file in files:
-      if file.endswith('.ckpt'):
+      if typ in file and file.endswith('.ckpt'):
         checkpoints.append(os.path.join(root, file))
 
   if not checkpoints:
